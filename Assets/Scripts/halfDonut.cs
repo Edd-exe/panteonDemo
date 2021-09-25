ad7aff765f4beb143ca2ba46c9a4f6b6a507d337
Edd-exe <e.demirgoz.exe@gmail.com>
2021-09-25 21:22:10 +0300

    9.25.2

--- Assets/Scripts/halfDonut.cs
+++ Assets/Scripts/halfDonut.cs
@@ -4,25 +4,51 @@ using UnityEngine;
 
 public class halfDonut : MonoBehaviour
 {   
+    public int HDPos;
     public float speed;
-    public float cooldown;
-    void Start()
-    {
-        StartCoroutine(rotateTimer());
-    }
+    int a;
+
+
 
     void Update()
-    {
-       
-    }
-
-    public IEnumerator rotateTimer()
-    {
-        while (true)
+    { 
+        if (HDPos == 0)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation,  Quaternion.Euler(90,0,0), speed * Time.deltaTime);
-            yield return new WaitForSeconds(cooldown); 
+            transform.rotation = Quaternion.Lerp(transform.rotation,  Quaternion.Euler(a,0,0), speed * Time.deltaTime);
+
+            if ( transform.localRotation.eulerAngles.x <= 1)
+            {
+                a = 90;
+            }   
+            if ( transform.localRotation.eulerAngles.x >= 89)
+            {
+                a = 0;
+            }
+        }
+
+        if (HDPos == 1)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation,  Quaternion.Euler(a,0,180), speed * Time.deltaTime);
+
+            if ( transform.localRotation.eulerAngles.x <= 1)
+            {
+                a = 90;
+            }   
+            if ( transform.localRotation.eulerAngles.x >= 89)
+            {
+                a = 0;
+            }
         }
         
+        
+        Debug.Log(transform.localRotation.eulerAngles.x);
+
     }
+
 }
+
+// transform.rotation = Quaternion.Lerp(transform.rotation,  Quaternion.Euler(0,0,0), speed * Time.deltaTime);
+// StartCoroutine(rotateTimer());
+// Mathf.Abs()
+// transform.localRotation.eulerAngles.x
+// transform.Rotate(0,0,0);
---
+++ Assets/Scripts/wallPaint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallPaint : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    static float CalculateFill ( Color[] colors , Color reference , float tolerance )
+    {
+        Vector3 target = new Vector3{ x=reference.r , y=reference.g , z=reference.b };
+        int numHits = 0;
+        const float sqrt_3 = 1.73205080757f;
+        
+        for( int i=0 ; i<colors.Length ; i++ )
+        {
+            Vector3 next = new Vector3{ x=colors[i].r , y=colors[i].g , z=colors[i].b };
+            float mag = Vector3.Magnitude( target - next ) / sqrt_3;
+            numHits += mag<=tolerance ? 1 : 0;
+        } 
+        return (float)numHits / (float)colors.Length;;
+    }
+    
+    static float CalculateSimilarity ( Color[] colors , Color reference )
+    {
+        Vector3 target = new Vector3{ x=reference.r , y=reference.g , z=reference.b };
+        float accu = 0;
+        const float sqrt_3 = 1.73205080757f;
+        
+        for( int i=0 ; i<colors.Length ; i++ )
+        {
+         Vector3 next = new Vector3{ x=colors[i].r , y=colors[i].g , z=colors[i].b };
+         accu += Vector3.Magnitude( target - next ) / sqrt_3;
+        }
+        return 1f - ( (float)accu / (float)colors.Length );
+    }
+
+     
+
+}
